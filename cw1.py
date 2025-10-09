5e89e76f056609ced53b21ccc22e238c85921cd9
python-programmer <nimaostad200048@gmail.com>
2025-10-09 15:19:08 +0330

    count added

--- cw1.py
+++ cw1.py
@@ -6,10 +6,13 @@ names_add = names.split(",")
 names_processed = [name for name in names_add if len(name) > 3 and name[0] == 'a' or name[0] == 'A']
 
 
-# ali amir nima
+# ali amir nima arman ahmad
 sorted_user = sorted(names_processed)
 with open('names.txt', 'w') as file:
-    file.write(str(sorted_user))
-
+    counter = 0
+    for name in sorted_user:
+        counter += 1
+        file.write(name + '\n')
+    file.write(f"Total number of names: {counter}")
 
 print(sorted_user)
