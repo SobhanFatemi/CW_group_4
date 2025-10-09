51a96bf181557339c2e850af790099b494393093
alialavi <ali.miralavi1382@gmail.com>
2025-10-09 15:01:55 +0330

    test

--- cw1.py
+++ cw1.py
@@ -5,5 +5,11 @@ names_add = names.split(",")
 
 names_processed = [name for name in names_add if len(name) > 3 and name[0] == 'a' or name[0] == 'A']
 
+
+# ali amir nima
 sorted_user = sorted(names_processed)
+with open('names.txt', 'w') as file:
+    file.write(str(sorted_user))
+
+
 print(sorted_user)
