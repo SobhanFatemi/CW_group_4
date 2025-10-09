228be58ee106e3eaa043a06b9c63590b131ec735
zahra-shayan <zhr.shayan81@gmail.com>
2025-10-09 14:25:47 +0200

    last

--- cw1.py
+++ cw1.py
@@ -8,11 +8,15 @@ names_processed = [name for name in names_add if len(name) > 3 and name[0] == 'a
 
 # ali amir nima arman ahmad
 sorted_user = sorted(names_processed)
-with open('names.txt', 'w') as file:
-    counter = 0
-    for name in sorted_user:
-        counter += 1
-        file.write(name + '\n')
-    file.write(f"Total number of names: {counter}")
 
+try: 
+    with open('names.txt', 'w') as file:
+        counter = 0
+        for name in sorted_user:
+            counter += 1
+            file.write(name + '\n')
+        file.write(f"Total number of names: {counter}")
+    print(f"file saved successfully with {counter} name")
+except FileNotFoundError as error :
+    print("error")
 print(sorted_user)
