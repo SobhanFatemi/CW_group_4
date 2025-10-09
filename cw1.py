3d81f7e65b6da9bc94077d2d0e6edbc0ee18d4fb
maryam <army1384m@gmail.com>
2025-10-09 14:51:15 +0330

    sorted done!

--- cw1.py
+++ cw1.py
@@ -1,7 +1,9 @@
 user_input = input("Please enter bunch of names seperated by ',': ")
 
-names = user_input.split(',')
+names = user_input.replace(" ", ",")
+names_add = names.split(",")
 
-print(names)
+names_processed = [name for name in names_add if len(name) > 3 and name[0] == 'a' or name[0] == 'A']
 
-names_processed = [name for name in names if len(name) > 3 and name[0] == 'a' or name[0] == 'A']
+sorted_user = sorted(names_processed)
+print(sorted_user)
