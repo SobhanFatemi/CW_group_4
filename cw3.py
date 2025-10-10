5ed2b8e2e39095c3a414fa0a563e5758c6859929
Sobhan Fatemi <sobhanfatemi55@gmail.com>
2025-10-10 15:35:26 +0330

    Pushed cw3 compeleted

--- cw3.py
+++ cw3.py
@@ -1,18 +1,39 @@
 from datetime import datetime
+import random
+
 def timer_decorator(func):
     def wrapper(*args, **kwargs):
         try:
             current = datetime.now()
             print(current)
-            result = func(*args, **kwargs)
-            return result
+            return func(*args, **kwargs)
         except:
             print("An error happened!")
             return None
     return wrapper
 
 @timer_decorator
-def work():
-    print("Working...")
+def simulate_weather(days, start_temp = 25):
+    try:
+        if days <= 0:
+            print("Positive numbers only!")
+            return None
+        temps = []
+        for day in range(days):
+            diff = random.randint(-2, 3)
+            temps.append(start_temp + diff)
+        
+        return temps
+    except TypeError:
+        print("Please enter a number!")
+        return None
 
-work()
+temps = simulate_weather(10)
+
+max_temp = max(temps)
+min_temp = min(temps)
+avg_temp = sum(temps)/len(temps)
+
+print(f"Maximum temp: {max_temp}")
+print(f"Minimum temp: {min_temp}")
+print(f"Average temp: {avg_temp}")
