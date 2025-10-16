77dac5e2ec1b63bfb3adf4d1d65fe54db08cb3c7
Sobhan Fatemi <sobhanfatemi55@gmail.com>
2025-10-16 20:01:04 +0330

    Changed repo's construction

--- cw4.py
+++
@@ -1,7 +0,0 @@
-prices = {"Apple": 60, "Banana": 20, "Orange": 45, "Grapes": 80}
-
-new_dictionary = {key: value for key, value in prices.items() if value > 50}
-
-new_dictionary2 = {key: value*0.9 for key, value in new_dictionary.items() if value > 50}
-
-print(new_dictionary2)
