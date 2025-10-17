ffb2878f0e37358c47d4cf957521c3bdc7dd3c20
Sobhan Fatemi <sobhanfatemi55@gmail.com>
2025-10-17 18:21:43 +0330

    week06/friday done up until cw6-2-4

--- week06/friday/cw6-2-3.py
+++ week06/friday/cw6-2-3.py
@@ -22,15 +22,15 @@ class Car:
     def display_info(self):
         print(f"Car ID: {self.car_id}\nModel: {self.model}\nYear: {self.year}\nAvailability: {self.is_available}")
     
-samand = Car(123456, "Soren", 1391, True)
 
-class RentalAgency(Car):
+class RentalAgency():
     def __init__(self, cars=[]):
         self.cars = cars
 
-    def add_car(self, car):
-        self.cars.append(car)
-        print(f"{car.model} was added!")
+    def add_car(self, *args):
+        for arg in args:
+            self.cars.append(arg)
+            print(f"{arg.model} was added!")
 
     def find_car(self, id):
         found = False
@@ -42,17 +42,19 @@ class RentalAgency(Car):
             print("Car not found!")
 
     def rent_car(self, car):
-        return super().return_car(car)
-    
+        car.rent_car()
     def return_car(self, car):
-        return super().return_car(car)
+        car.return_car()
     
     def display_info(self, car):
-        return super().display_info(car)
+        car.display_info()
+
+samand = Car(347198, "Soren", 1391, True)
+peugeot = Car(859303, "206", 1390, True)
         
 agency = RentalAgency()
-agency.add_car(samand)
-agency.find_car(123456)
-agency.find_car(12345)
+agency.add_car(samand, peugeot)
+agency.find_car(347198)
+agency.find_car(859303)
 agency.rent_car(samand)
 agency.display_info(samand)
